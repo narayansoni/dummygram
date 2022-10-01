a5ed20d21438100b9328019f9373d5df4360664a
narayan954 <77617189+narayan954@users.noreply.github.com>
2022-10-01 07:09:07 +0000

    Deploying to gh-pages from  @ 44fe4dec8d389b1a16ea2883cd21452044e5344d 🚀

---
+++ precache-manifest.75eecb76d5934b707ce34bd80db309f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e402f5d711d2b708e27d",
+    "url": "/static/css/main.f9f096e2.chunk.css"
+  },
+  {
+    "revision": "e402f5d711d2b708e27d",
+    "url": "/static/js/main.e402f5d7.chunk.js"
+  },
+  {
+    "revision": "50a5542c7c63028d378b",
+    "url": "/static/js/1.50a5542c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c94f30c96f0d1149cab2938516064979",
+    "url": "/index.html"
+  }
+];
